35456cfc43969245f0eae5e82a63e4bf2582ad5f
J. David Mendoza <jdmendoza@um.edu.mx>
2012-01-24 17:52:44 -0600

    SQLs de seguridad para mysql y postgresql

---
+++ src/test/resources/mysql.sql
@@ -0,0 +1,29 @@
+drop table if exists authorities;
+drop table if exists users;
+
+create table users (
+    id bigint not null auto_increment
+    , username varchar(32) unique not null
+    , password varchar(254) not null
+    , enabled boolean not null default true
+    , primary key (id)
+);
+
+create table authorities (
+    id bigint not null auto_increment
+    , username varchar(32) not null
+    , authority varchar(32) not null
+    , primary key (id)
+    , unique key authorities_key_01 (username, authority)
+    , index authorities_idx_01 (username)
+    , constraint authorities_fk_01 foreign key(username) references users(username)
+);
+
+insert into users(username, password) values('david',md5('david'));
+
+insert into authorities(username, authority) values('david','ROLE_ADMIN');
+insert into authorities(username, authority) values('david','ROLE_USER');
+
+insert into users(username, password, enabled) values('daniel',md5('daniel'),true);
+
+insert into authorities(username, authority) values('daniel','ROLE_USER');
---
+++ src/test/resources/postgres.sql
@@ -0,0 +1,32 @@
+drop table if exists authorities cascade;
+drop sequence if exists authorities_seq;
+drop table if exists users cascade;
+drop sequence if exists users_seq;
+
+create sequence users_seq;
+create table users (
+    id bigint not null default nextval('users_seq')
+    , username varchar(32) unique not null
+    , password varchar(254) not null
+    , enabled boolean not null default true
+    , primary key (id)
+);
+
+create sequence authorities_seq;
+create table authorities (
+    id bigint not null default nextval('authorities_seq')
+    , username varchar(32) not null
+    , authority varchar(32) not null
+    , primary key (id)
+    , unique (username, authority)
+    , foreign key(username) references users(username)
+);
+
+insert into users(username, password) values('david',md5('david'));
+
+insert into authorities(username, authority) values('david','ROLE_ADMIN');
+insert into authorities(username, authority) values('david','ROLE_USER');
+
+insert into users(username, password, enabled) values('daniel',md5('daniel'),true);
+
+insert into authorities(username, authority) values('daniel','ROLE_USER');
