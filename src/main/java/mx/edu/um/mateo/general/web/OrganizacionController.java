19dc0c8280e2516660f0b7d9b1b2c2ef0c4dc7e4
J. David Mendoza <jdmendoza@um.edu.mx>
2012-02-05 21:27:57 -0600

    Trabajando con controlador de organizaciones [#24076175]

---
+++ src/main/java/mx/edu/um/mateo/general/web/OrganizacionController.java
@@ -0,0 +1,347 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2012 jdmr.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package mx.edu.um.mateo.general.web;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.util.ByteArrayDataSource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import mx.edu.um.mateo.general.dao.OrganizacionDao;
+import mx.edu.um.mateo.general.model.Organizacion;
+import mx.edu.um.mateo.general.utils.SpringSecurityUtils;
+import mx.edu.um.mateo.general.utils.UltimoException;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.exception.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ *
+ * @author jdmr
+ */
+@Controller
+@RequestMapping("/admin/organizacion")
+public class OrganizacionController {
+
+    private static final Logger log = LoggerFactory.getLogger(OrganizacionController.class);
+    @Autowired
+    private OrganizacionDao organizacionDao;
+    @Autowired
+    private JavaMailSender mailSender;
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
+
+    @RequestMapping
+    public String lista(HttpServletRequest request, HttpServletResponse response,
+            @RequestParam(required = false) String filtro,
+            @RequestParam(required = false) Long pagina,
+            @RequestParam(required = false) String tipo,
+            @RequestParam(required = false) String correo,
+            @RequestParam(required = false) String order,
+            @RequestParam(required = false) String sort,
+            Model modelo) {
+        log.debug("Mostrando lista de organizaciones");
+        Map<String, Object> params = new HashMap<>();
+        if (StringUtils.isNotBlank(filtro)) {
+            params.put("filtro", filtro);
+        }
+        if (pagina != null) {
+            params.put("pagina", pagina);
+            modelo.addAttribute("pagina", pagina);
+        } else {
+            pagina = 1L;
+            modelo.addAttribute("pagina", pagina);
+        }
+        if (StringUtils.isNotBlank(order)) {
+            params.put("order", order);
+            params.put("sort", sort);
+        }
+
+        if (StringUtils.isNotBlank(tipo)) {
+            params.put("reporte", true);
+            params = organizacionDao.lista(params);
+            try {
+                generaReporte(tipo, (List<Organizacion>) params.get("organizaciones"), response);
+                return null;
+            } catch (JRException | IOException e) {
+                log.error("No se pudo generar el reporte", e);
+            }
+        }
+
+        if (StringUtils.isNotBlank(correo)) {
+            params.put("reporte", true);
+            params = organizacionDao.lista(params);
+
+            params.remove("reporte");
+            try {
+                enviaCorreo(correo, (List<Organizacion>) params.get("organizaciones"), request);
+            } catch (JRException | MessagingException e) {
+                log.error("No se pudo enviar el reporte por correo", e);
+            }
+        }
+        params = organizacionDao.lista(params);
+        modelo.addAttribute("organizaciones", params.get("organizaciones"));
+
+        // inicia paginado
+        Long cantidad = (Long) params.get("cantidad");
+        Integer max = (Integer) params.get("max");
+        Long cantidadDePaginas = cantidad / max;
+        List<Long> paginas = new ArrayList<>();
+        for (long i = 1; i <= cantidadDePaginas + 1; i++) {
+            paginas.add(i);
+        }
+        List<Organizacion> organizaciones = (List<Organizacion>) params.get("organizaciones");
+        Long primero = ((pagina - 1) * max) + 1;
+        Long ultimo = primero + (organizaciones.size() - 1);
+        String[] paginacion = new String[]{primero.toString(), ultimo.toString(), cantidad.toString()};
+        modelo.addAttribute("paginacion", paginacion);
+        modelo.addAttribute("paginas", paginas);
+        // termina paginado
+
+        return "admin/organizacion/lista";
+    }
+
+    @RequestMapping("/ver/{id}")
+    public String ver(@PathVariable Long id, Model modelo) {
+        log.debug("Mostrando organizacion {}", id);
+        Organizacion organizacion = organizacionDao.obtiene(id);
+
+        modelo.addAttribute("organizacion", organizacion);
+
+        return "admin/organizacion/ver";
+    }
+
+    @RequestMapping("/nuevo")
+    public String nuevo(Model modelo) {
+        log.debug("Nuevo organizacion");
+        Organizacion organizacion = new Organizacion();
+        modelo.addAttribute("organizacion", organizacion);
+        return "admin/organizacion/nuevo";
+    }
+
+    @RequestMapping(value = "/crea", method = RequestMethod.POST)
+    public String crea(HttpServletRequest request, HttpServletResponse response, @Valid Organizacion organizacion, BindingResult bindingResult, Errors errors, Model modelo, RedirectAttributes redirectAttributes) {
+        for (String nombre : request.getParameterMap().keySet()) {
+            log.debug("Param: {} : {}", nombre, request.getParameterMap().get(nombre));
+        }
+        if (bindingResult.hasErrors()) {
+            log.debug("Hubo algun error en la forma, regresando");
+            return "admin/organizacion/nuevo";
+        }
+
+        try {
+            organizacion = organizacionDao.crea(organizacion, null);
+        } catch (ConstraintViolationException e) {
+            log.error("No se pudo crear al organizacion", e);
+            errors.rejectValue("codigo", "campo.duplicado.message", new String[]{"codigo"}, null);
+            errors.rejectValue("nombre", "campo.duplicado.message", new String[]{"nombre"}, null);
+            return "admin/codigo/nuevo";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "organizacion.creada.message");
+        redirectAttributes.addFlashAttribute("messageAttrs", new String[]{organizacion.getNombre()});
+
+        return "redirect:/admin/organizacion/ver/" + organizacion.getId();
+    }
+
+    @RequestMapping("/edita/{id}")
+    public String edita(@PathVariable Long id, Model modelo) {
+        log.debug("Edita organizacion {}", id);
+        Organizacion organizacion = organizacionDao.obtiene(id);
+        modelo.addAttribute("organizacion", organizacion);
+        return "admin/organizacion/edita";
+    }
+
+    @RequestMapping(value = "/actualiza", method = RequestMethod.POST)
+    public String actualiza(HttpServletRequest request, @Valid Organizacion organizacion, BindingResult bindingResult, Errors errors, Model modelo, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            log.error("Hubo algun error en la forma, regresando");
+            return "admin/organizacion/edita";
+        }
+
+        try {
+            organizacion = organizacionDao.actualiza(organizacion, null);
+        } catch (ConstraintViolationException e) {
+            log.error("No se pudo crear al organizacion", e);
+            errors.rejectValue("codigo", "campo.duplicado.message", new String[]{"codigo"}, null);
+            errors.rejectValue("nombre", "campo.duplicado.message", new String[]{"nombre"}, null);
+            return "admin/organizacion/nuevo";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "organizacion.actualizada.message");
+        redirectAttributes.addFlashAttribute("messageAttrs", new String[]{organizacion.getNombre()});
+
+        return "redirect:/admin/organizacion/ver/" + organizacion.getId();
+    }
+
+    @RequestMapping(value = "/elimina", method = RequestMethod.POST)
+    public String elimina(@RequestParam Long id, Model modelo, @ModelAttribute Organizacion organizacion, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        log.debug("Elimina organizacion");
+        try {
+            String nombre = organizacionDao.elimina(id);
+            redirectAttributes.addFlashAttribute("message", "organizacion.eliminado.message");
+            redirectAttributes.addFlashAttribute("messageAttrs", new String[]{nombre});
+        } catch (UltimoException e) {
+            log.error("No se pudo eliminar el organizacion " + id, e);
+            bindingResult.addError(new ObjectError("organizacion", new String[]{"ultima.organizacion.no.eliminada.message"}, null, null));
+            return "admin/organizacion/ver";
+        } catch (Exception e) {
+            log.error("No se pudo eliminar el organizacion " + id, e);
+            bindingResult.addError(new ObjectError("organizacion", new String[]{"organizacion.no.eliminada.message"}, null, null));
+            return "admin/organizacion/ver";
+        }
+
+        return "redirect:/admin/organizacion";
+    }
+
+    private void generaReporte(String tipo, List<Organizacion> organizaciones, HttpServletResponse response) throws JRException, IOException {
+        log.debug("Generando reporte {}", tipo);
+        byte[] archivo = null;
+        switch (tipo) {
+            case "PDF":
+                archivo = generaPdf(organizaciones);
+                response.setContentType("application/pdf");
+                response.addHeader("Content-Disposition", "attachment; filename=organizaciones.pdf");
+                break;
+            case "CSV":
+                archivo = generaCsv(organizaciones);
+                response.setContentType("text/csv");
+                response.addHeader("Content-Disposition", "attachment; filename=organizaciones.csv");
+                break;
+            case "XLS":
+                archivo = generaXls(organizaciones);
+                response.setContentType("application/vnd.ms-excel");
+                response.addHeader("Content-Disposition", "attachment; filename=organizaciones.xls");
+        }
+        if (archivo != null) {
+            response.setContentLength(archivo.length);
+            try (BufferedOutputStream bos = new BufferedOutputStream(response.getOutputStream())) {
+                bos.write(archivo);
+                bos.flush();
+            }
+        }
+
+    }
+
+    private void enviaCorreo(String tipo, List<Organizacion> organizaciones, HttpServletRequest request) throws JRException, MessagingException {
+        log.debug("Enviando correo {}", tipo);
+        byte[] archivo = null;
+        String tipoContenido = null;
+        switch (tipo) {
+            case "PDF":
+                archivo = generaPdf(organizaciones);
+                tipoContenido = "application/pdf";
+                break;
+            case "CSV":
+                archivo = generaCsv(organizaciones);
+                tipoContenido = "text/csv";
+                break;
+            case "XLS":
+                archivo = generaXls(organizaciones);
+                tipoContenido = "application/vnd.ms-excel";
+        }
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(request.getUserPrincipal().getName());
+        String titulo = messageSource.getMessage("organizacion.lista.label", null, request.getLocale());
+        helper.setSubject(messageSource.getMessage("envia.correo.titulo.message", new String[]{titulo}, request.getLocale()));
+        helper.setText(messageSource.getMessage("envia.correo.contenido.message", new String[]{titulo}, request.getLocale()), true);
+        helper.addAttachment(titulo + "." + tipo, new ByteArrayDataSource(archivo, tipoContenido));
+        mailSender.send(message);
+    }
+
+    private byte[] generaPdf(List organizaciones) throws JRException {
+        Map<String, Object> params = new HashMap<>();
+        JasperDesign jd = JRXmlLoader.load(this.getClass().getResourceAsStream("/mx/edu/um/mateo/general/reportes/organizaciones.jrxml"));
+        JasperReport jasperReport = JasperCompileManager.compileReport(jd);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JRBeanCollectionDataSource(organizaciones));
+        byte[] archivo = JasperExportManager.exportReportToPdf(jasperPrint);
+
+        return archivo;
+    }
+
+    private byte[] generaCsv(List organizaciones) throws JRException {
+        Map<String, Object> params = new HashMap<>();
+        JRCsvExporter exporter = new JRCsvExporter();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        JasperDesign jd = JRXmlLoader.load(this.getClass().getResourceAsStream("/mx/edu/um/mateo/general/reportes/organizaciones.jrxml"));
+        JasperReport jasperReport = JasperCompileManager.compileReport(jd);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JRBeanCollectionDataSource(organizaciones));
+        exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+        exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, byteArrayOutputStream);
+        exporter.exportReport();
+        byte[] archivo = byteArrayOutputStream.toByteArray();
+
+        return archivo;
+    }
+
+    private byte[] generaXls(List organizaciones) throws JRException {
+        Map<String, Object> params = new HashMap<>();
+        JRXlsExporter exporter = new JRXlsExporter();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        JasperDesign jd = JRXmlLoader.load(this.getClass().getResourceAsStream("/mx/edu/um/mateo/general/reportes/organizaciones.jrxml"));
+        JasperReport jasperReport = JasperCompileManager.compileReport(jd);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JRBeanCollectionDataSource(organizaciones));
+        exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+        exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, byteArrayOutputStream);
+        exporter.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
+        exporter.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE);
+        exporter.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_COLUMNS, Boolean.TRUE);
+        exporter.setParameter(JRXlsExporterParameter.IS_COLLAPSE_ROW_SPAN, Boolean.TRUE);
+        exporter.setParameter(JRXlsExporterParameter.IGNORE_PAGE_MARGINS, Boolean.TRUE);
+        exporter.setParameter(JRXlsExporterParameter.IS_ONE_PAGE_PER_SHEET, Boolean.FALSE);
+        exporter.exportReport();
+        byte[] archivo = byteArrayOutputStream.toByteArray();
+
+        return archivo;
+    }
+}
