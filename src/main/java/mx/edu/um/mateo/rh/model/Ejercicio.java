435159945746ccc8df2a0b45da87f2f6aa96647b
Evijair Nuñez <nujev2008@gmail.com>
2012-02-17 11:46:02 -0600

    todas las pruebas de ejercicio y ctaMayor [#24791065]

--- src/main/java/mx/edu/um/mateo/rh/model/Ejercicio.java
+++ src/main/java/mx/edu/um/mateo/rh/model/Ejercicio.java
@@ -46,6 +46,14 @@ public class Ejercicio implements Serializable {
         this.status = status;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     /**
      * @return the nombre
      */
