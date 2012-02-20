5aba93a6182347403e4fda8e88e3d49bc65c5970
Evijair Nuñez <nujev2008@gmail.com>
2012-02-20 10:41:35 -0600

    Agregar archivo de constantes

---
+++ src/main/java/mx/edu/um/mateo/Constantes.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.um.mateo;
+
+/**
+ *
+ * @author nujev
+ */
+public class Constantes {
+
+    public final String STATUS_ACTIVO = "A";
+}
