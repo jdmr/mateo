e910fa9bdd1b5ec6d855b289f30bd08a550d57f9
J. David Mendoza <jdmendoza@um.edu.mx>
2012-06-24 07:39:55 -0500

    Comente import de sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
-begin;
+-- begin;
 -- alter table xlotes_entrada drop column last_updated;
 -- alter table xlotes_salida  drop column last_updated;
 -- alter table xlotes_entrada alter column version type integer;
 -- alter table xlotes_salida  alter column version type integer;
-alter table cancelaciones_almacen drop column last_updated;
-commit;
+-- alter table cancelaciones_almacen drop column last_updated;
+-- commit;
