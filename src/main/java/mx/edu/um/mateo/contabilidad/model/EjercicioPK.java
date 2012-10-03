a1da0d23e3e1939f90d55dca850a19a5ac2c8d19
J. David Mendoza <jdmendoza@um.edu.mx>
2012-10-03 16:02:45 -0500

    Arregle problema suscitado de integrar Tipos de Activo a Cuentas de Mayor

--- src/main/java/mx/edu/um/mateo/contabilidad/model/EjercicioPK.java
+++ src/main/java/mx/edu/um/mateo/contabilidad/model/EjercicioPK.java
@@ -25,96 +25,92 @@ package mx.edu.um.mateo.contabilidad.model;
 
 import java.io.Serializable;
 import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.ManyToOne;
-
 import mx.edu.um.mateo.general.model.Organizacion;
 
 /**
- * 
+ *
  * @author J. David Mendoza <jdmendoza@um.edu.mx>
  */
 @Embeddable
 public class EjercicioPK implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 8523273562997747556L;
-	@Column(name = "id_ejercicio", nullable = false, length = 8)
-	private String idEjercicio;
-	@ManyToOne(optional = false)
-	private Organizacion organizacion;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 8523273562997747556L;
+    @Column(name = "id_ejercicio", nullable = false, length = 8)
+    private String idEjercicio;
+    @ManyToOne(optional = false)
+    private Organizacion organizacion;
 
-	public EjercicioPK() {
-	}
+    public EjercicioPK() {
+    }
 
-	public EjercicioPK(String idEjercicio, Organizacion organizacion) {
-		this.idEjercicio = idEjercicio;
-		this.organizacion = organizacion;
-	}
+    public EjercicioPK(String idEjercicio, Organizacion organizacion) {
+        this.idEjercicio = idEjercicio;
+        this.organizacion = organizacion;
+    }
 
-	/**
-	 * @return the idEjercicio
-	 */
-	public String getIdEjercicio() {
-		return idEjercicio;
-	}
+    /**
+     * @return the idEjercicio
+     */
+    public String getIdEjercicio() {
+        return idEjercicio;
+    }
 
-	/**
-	 * @param idEjercicio
-	 *            the idEjercicio to set
-	 */
-	public void setIdEjercicio(String idEjercicio) {
-		this.idEjercicio = idEjercicio;
-	}
+    /**
+     * @param idEjercicio the idEjercicio to set
+     */
+    public void setIdEjercicio(String idEjercicio) {
+        this.idEjercicio = idEjercicio;
+    }
 
-	/**
-	 * @return the organizacion
-	 */
-	public Organizacion getOrganizacion() {
-		return organizacion;
-	}
+    /**
+     * @return the organizacion
+     */
+    public Organizacion getOrganizacion() {
+        return organizacion;
+    }
 
-	/**
-	 * @param organizacion
-	 *            the organizacion to set
-	 */
-	public void setOrganizacion(Organizacion organizacion) {
-		this.organizacion = organizacion;
-	}
+    /**
+     * @param organizacion the organizacion to set
+     */
+    public void setOrganizacion(Organizacion organizacion) {
+        this.organizacion = organizacion;
+    }
 
-	@Override
-	public int hashCode() {
-		int hash = 7;
-		hash = 17 * hash + Objects.hashCode(this.idEjercicio);
-		hash = 17 * hash + Objects.hashCode(this.organizacion);
-		return hash;
-	}
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.idEjercicio);
+        hash = 17 * hash + Objects.hashCode(this.organizacion);
+        return hash;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		final EjercicioPK other = (EjercicioPK) obj;
-		if (!Objects.equals(this.idEjercicio, other.idEjercicio)) {
-			return false;
-		}
-		if (!Objects.equals(this.organizacion, other.organizacion)) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final EjercicioPK other = (EjercicioPK) obj;
+        if (!Objects.equals(this.idEjercicio, other.idEjercicio)) {
+            return false;
+        }
+        if (!Objects.equals(this.organizacion, other.organizacion)) {
+            return false;
+        }
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return "EjercicioPK{" + "idEjercicio=" + idEjercicio
-				+ ", organizacion=" + organizacion + '}';
-	}
+    @Override
+    public String toString() {
+        return "EjercicioPK{" + "idEjercicio=" + idEjercicio
+                + ", organizacion=" + organizacion + '}';
+    }
 }
