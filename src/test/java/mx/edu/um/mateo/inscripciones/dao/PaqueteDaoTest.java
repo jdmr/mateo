5027728aba00cee50d8a3d4b4ad36f431d560cc7
sam <samuel.9401@gmail.com>
2013-02-26 11:24:03 -0600

    Paquetes [#44816783] DaoTest funcionando

--- src/test/java/mx/edu/um/mateo/inscripciones/dao/PaqueteDaoTest.java
+++ src/test/java/mx/edu/um/mateo/inscripciones/dao/PaqueteDaoTest.java
@@ -4,21 +4,27 @@
  */
 package mx.edu.um.mateo.inscripciones.dao;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import mx.edu.um.mateo.general.model.Empresa;
 import mx.edu.um.mateo.general.model.Organizacion;
+import mx.edu.um.mateo.general.model.Rol;
+import mx.edu.um.mateo.general.model.Usuario;
 import mx.edu.um.mateo.general.utils.Constantes;
 import mx.edu.um.mateo.inscripciones.model.Paquete;
 import mx.edu.um.mateo.inscripciones.model.TiposBecas;
+import mx.edu.um.mateo.inventario.model.Almacen;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -70,28 +76,49 @@ public class PaqueteDaoTest {
      */
     @Test
     public void testGetPaquetes() {
-        Organizacion organizacion = new Organizacion("tst-01", "test-01", "test-01");
+   
+
+         Organizacion organizacion = new Organizacion("tst-01", "test-01", "test-01");
         currentSession().save(organizacion);
+        assertNotNull(organizacion.getId());
         Empresa empresa = new Empresa("tst-01", "test-01", "test-01", "000000000001", organizacion);
         currentSession().save(empresa);
+        assertNotNull(empresa.getId());
+        Rol rol = new Rol("ROLE_TEST");
+        currentSession().save(rol);
+        assertNotNull(rol.getId());
+        Set<Rol> roles = new HashSet<>();
+        roles.add(rol);
+        Almacen almacen = new Almacen("TST", "TEST", empresa);
+        currentSession().save(almacen);
+        assertNotNull(almacen.getId());
+        Usuario usuario = new Usuario("bugs@um.edu.mx", "apPaterno", "apMaterno", "TEST-01", "TEST-01");
+        usuario.setEmpresa(empresa);
+        usuario.setAlmacen(almacen);
+        usuario.setRoles(roles);
+        currentSession().save(usuario);
+        Long id = usuario.getId();
+        assertNotNull(id);
+        Paquete paquete=null;
         for (int i = 0; i < 20; i++) {
-            Paquete paquete = new Paquete();
+            paquete = new Paquete();
             paquete.setAcfe("s");
             paquete.setDescripcion("test");
             paquete.setEnsenanza(new Double("1.2"));
             paquete.setInternado(new Double("2.2"));
             paquete.setMatricula(new Double("3.6"));
             paquete.setNombre("Test");
-            paquete.setEmpresa(empresa);
-            currentSession().save(paquete);
-//        currentSession().save(paquete);
+            instance.graba(paquete, usuario);
+            assertNotNull(paquete.getId());
         }
 
-        Map<String, Object> params = null;
+        Map<String, Object> params;
+        params = new TreeMap<>();
+        params.put("empresa", empresa.getId());
         Map<String, Object> result = instance.getPaquetes(params);
-        assertNotNull(result.get("paquetes"));
+        assertNotNull(result.get(Constantes.CONTAINSKEY_PAQUETES));
         assertNotNull(result.get(Constantes.CONTAINSKEY_CANTIDAD));
-        assertEquals(10, ((List<TiposBecas>) result.get("paquetes")).size());
+        assertEquals(10, ((List<TiposBecas>) result.get(Constantes.CONTAINSKEY_PAQUETES)).size());
         assertEquals(20, ((Long) result.get(Constantes.CONTAINSKEY_CANTIDAD)).intValue());
     }
 
@@ -127,8 +154,25 @@ public class PaqueteDaoTest {
     public void testSavePaquete() {
         Organizacion organizacion = new Organizacion("tst-01", "test-01", "test-01");
         currentSession().save(organizacion);
+        assertNotNull(organizacion.getId());
         Empresa empresa = new Empresa("tst-01", "test-01", "test-01", "000000000001", organizacion);
         currentSession().save(empresa);
+        assertNotNull(empresa.getId());
+        Rol rol = new Rol("ROLE_TEST");
+        currentSession().save(rol);
+        assertNotNull(rol.getId());
+        Set<Rol> roles = new HashSet<>();
+        roles.add(rol);
+        Almacen almacen = new Almacen("TST", "TEST", empresa);
+        currentSession().save(almacen);
+        assertNotNull(almacen.getId());
+        Usuario usuario = new Usuario("bugs@um.edu.mx", "apPaterno", "apMaterno", "TEST-01", "TEST-01");
+        usuario.setEmpresa(empresa);
+        usuario.setAlmacen(almacen);
+        usuario.setRoles(roles);
+        currentSession().save(usuario);
+        Long id = usuario.getId();
+        assertNotNull(id);
         String nombre = "nombre";
         Paquete paquete = new Paquete();
         paquete.setAcfe("s");
@@ -138,7 +182,7 @@ public class PaqueteDaoTest {
         paquete.setMatricula(new Double("3.6"));
         paquete.setNombre(nombre);
         paquete.setEmpresa(empresa);
-        currentSession().save(paquete);
+        instance.graba(paquete, usuario);
         assertNotNull(paquete.getId());
     }
 
