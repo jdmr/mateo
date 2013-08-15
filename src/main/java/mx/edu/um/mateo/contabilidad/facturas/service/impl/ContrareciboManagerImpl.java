2383e1a098d65b9b30bab2e344bb0e11d819d5c9
sam <samuel.9401@gmail.com>
2013-08-15 15:36:42 -0500

    Contrarecibo[#54244502]manager creado, creando metodo para consulta de contrarecibos

---
+++ src/main/java/mx/edu/um/mateo/contabilidad/facturas/service/impl/ContrareciboManagerImpl.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.um.mateo.contabilidad.facturas.service.impl;
+
+import java.util.Map;
+import mx.edu.um.mateo.contabilidad.facturas.dao.ContrareciboDao;
+import mx.edu.um.mateo.contabilidad.facturas.model.Contrarecibo;
+import mx.edu.um.mateo.contabilidad.facturas.model.InformeEmpleadoDetalle;
+import mx.edu.um.mateo.contabilidad.facturas.service.ContrareciboManager;
+import mx.edu.um.mateo.general.model.Usuario;
+import mx.edu.um.mateo.general.utils.AutorizacionCCPlInvalidoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author develop
+ */
+@Transactional
+@Service
+public class ContrareciboManagerImpl implements ContrareciboManager {
+
+    @Autowired
+    private ContrareciboDao dao;
+
+    @Override
+    public Map<String, Object> lista(Map<String, Object> params) {
+        return dao.lista(params);
+    }
+
+    @Override
+    public void graba(Contrarecibo contrarecibo, Usuario usuario) {
+        dao.crea(contrarecibo, usuario);
+    }
+}
