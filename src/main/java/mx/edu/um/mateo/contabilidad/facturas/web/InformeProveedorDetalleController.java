cdea413453b4417d95b603cbfadd47da927f8615
sam <samuel.9401@gmail.com>
2013-10-21 10:18:39 -0500

    jsp colocado en el directorio correcto [#59144116] corregido]

--- src/main/java/mx/edu/um/mateo/contabilidad/facturas/web/InformeProveedorDetalleController.java
+++ src/main/java/mx/edu/um/mateo/contabilidad/facturas/web/InformeProveedorDetalleController.java
@@ -591,7 +591,7 @@ public class InformeProveedorDetalleController extends BaseController {
         log.debug("Pagina{}", pagina);
         // termina paginado
 
-        return "/factura/revisaProveedor/detalles";
+        return "/factura/informeProveedorDetalle/detalles";
     }
 
     @RequestMapping("/ver/{id}")
@@ -896,28 +896,28 @@ public class InformeProveedorDetalleController extends BaseController {
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE, "clabe.no.coincide");
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE_ATTRS, new String[]{e.getMessage()});
                 }
-                return "redirect:/factura/revisaProveedor/detalles";
+                return "redirect:/factura/informeProveedorDetalle/detalles";
             } catch (ProveedorNoCoincideException e) {
                 log.debug("el proveedor de la factura con id= {} no coincide", e);
                 if (e != null) {
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE, "proveedor.no.coincide");
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE_ATTRS, new String[]{e.getMessage()});
                 }
-                return "redirect:/factura/revisaProveedor/detalles";
+                return "redirect:/factura/informeProveedorDetalle/detalles";
             } catch (BancoNoCoincideException e) {
                 log.debug("el banco de la factura con id= {} no coincide", e);
                 if (e != null) {
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE, "banco.no.coincide");
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE_ATTRS, new String[]{e.getMessage()});
                 }
-                return "redirect:/factura/revisaProveedor/detalles";
+                return "redirect:/factura/informeProveedorDetalle/detalles";
             } catch (CuentaChequeNoCoincideException e) {
                 log.debug("la cuenta de la factura con id= {} no coincide", e);
                 if (e != null) {
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE, "cuenta.no.coincide");
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE_ATTRS, new String[]{e.getMessage()});
                 }
-                return "redirect:/factura/revisaProveedor/detalles";
+                return "redirect:/factura/informeProveedorDetalle/detalles";
 
             } catch (FormaPagoNoCoincideException e) {
                 log.debug("la forma de pago de la factura con id= {} no coincide", e);
@@ -925,7 +925,7 @@ public class InformeProveedorDetalleController extends BaseController {
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE, "cuenta.no.coincide");
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE_ATTRS, new String[]{e.getMessage()});
                 }
-                return "redirect:/factura/revisaProveedor/detalles";
+                return "redirect:/factura/informeProveedorDetalle/detalles";
             }
 
         }
@@ -939,7 +939,7 @@ public class InformeProveedorDetalleController extends BaseController {
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE, "banco.no.coincide");
                     redirectAttributes.addFlashAttribute(Constantes.CONTAINSKEY_MESSAGE_ATTRS, new String[]{e.getMessage()});
                 }
-                return "redirect:/factura/revisaProveedor/detalles";
+                return "redirect:/factura/informeProveedorDetalle/detalles";
             }
         }
 
